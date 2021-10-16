e473b395d65af7aeec836ce7e0b4082e441f6ab9
Danesh Rajasolan <daneshpbc@gmail.com>
2021-10-16 17:02:43 -0400

    add unit tests for carousels

--- TestApp/TestApp/ContentView.swift
+++ TestApp/TestApp/ContentView.swift
@@ -13,8 +13,9 @@ struct ContentView: View {
         let car1 = CarouselItem.sample1
         let car2 = CarouselItem.sample2
         let car3 = CarouselItem.sample3
+        let car4 = CarouselItem.sample4
 
-        Carousel(cardItems: [car1, car3, car2], defaultButtonTitle: "Next", lastPageButtonTitle: "Start Again")
+        Carousel(cardItems: [car1, car3, car2, car4], defaultButtonTitle: "Next", lastPageButtonTitle: "Start Again")
     }
 }
 
